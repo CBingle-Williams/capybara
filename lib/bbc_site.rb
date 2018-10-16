8aa584d9928d1b02a1bd8ec33baf39fc508a84d7
Evie Skinner <eskinner@spartaglobal.com>
2018-10-16 22:45:28 +0100

    first real commit

---
+++ lib/bbc_site.rb
@@ -0,0 +1,14 @@
+require_relative 'pages/bbc_homepage.rb'
+require_relative 'pages/bbc_sign_in_pages.rb'
+
+class BBCSite
+
+    def bbc_homepage
+        BBCHomepage.new
+    end
+
+    def bbc_sign_in_page
+        BBCSignInPage.new
+    end
+
+end
